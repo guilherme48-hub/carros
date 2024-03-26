1ebb10a2edee094b5bf667c6e4644a0b9e455ca6
Guilherme Santos Gomes <guistsgomes07@gmail.com>
2024-03-25 21:08:31 -0300

    segundo commit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import iconsedans from "./assets/icon-sedans.svg"
+import iconsuvs from "./assets/icon-suvs.svg"
+import iconluxury from "./assets/icon-luxury.svg"
+
 export default function App () {
   return (
-    <h1>hello monkey</h1>
+    <main>
+      <section>
+        <div>
+          <img src={iconsedans} alt="icone dos carros sedan" />
+          <h2>SEDANS</h2>
+          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans">veja mais</a>
+        </div>
+
+        <div>
+        <img src={iconsuvs} alt="icone dos carros suvs" />
+          <h2>SUVS</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road</p>
+          <a href="https://www.webmotors.com.br/suvs">veja mais</a>
+        </div>
+
+        <div>
+        <img src={iconluxury} alt="icone dos carros luxury" />
+          <h2>LUXURY</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-novos/estoque/lamborghini?tipoveiculo=carros-novos&marca1=LAMBORGHINI">veja mais</a>
+        </div>
+      </section>
+    </main>
   )
 }
