30d052031ade78829a2df445e307e6f541651500
Guilherme Santos Gomes <guistsgomes07@gmail.com>
2024-03-26 21:04:07 -0300

    acabou

--- src/components/Luxury.jsx
+++ src/components/Luxury.jsx
@@ -2,11 +2,11 @@ import iconluxury from "../assets/icon-luxury.svg"
 
 export default function Luxury() {
     return (
-        <div className="bg-dark-green p-12 max-lg rounded-b-lg lg:rounded">
+        <section className="bg-dark-green p-12 max-lg rounded-b-lg lg:rounded">
         <img src={iconluxury} alt="icone dos carros luxury" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxury</h2>
           <p className="font-lexend mb-7 text-offwhitebg lg-mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
           <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-dark-green  border-2 borde-offwhitebg hover:bg-dark-green hover:text-offwhitebg" href="https://www.webmotors.com.br/carros-novos/estoque/lamborghini?tipoveiculo=carros-novos&marca1=LAMBORGHINI" target="_blank">veja mais</a>
-        </div>
+        </section>
     )
 }
--- src/components/Suvs.jsx
+++ src/components/Suvs.jsx
@@ -2,11 +2,11 @@ import iconsuvs from "../assets/icon-suvs.svg"
 
 export default function Suvs() {
     return (
-        <div className="bg-green p-12">
+        <section className="bg-green p-12">
         <img src={iconsuvs} alt="icone dos carros suvs" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Suvs</h2>
           <p className="font-lexend mb-7 text-offwhitebg lg-mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road</p>
           <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green border-2 borde-offwhitebg hover:bg-green hover:text-offwhitebg" href="https://www.webmotors.com.br/suvs" target="_blank">veja mais</a>
-        </div>
+        </section>
     )
 }
